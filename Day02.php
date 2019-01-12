41a6568ea4c260bb69059e5fd2bec83d47d46a8d
phosfox <mordredur@googlemail.com>
2019-01-12 17:43:49 +0100

    Solved day2 part2

--- Day02.php
+++ Day02.php
@@ -21,6 +21,45 @@ class Day02
         }
     }
 
+    private function solvePart2()
+    {
+        foreach ($this->input as $str) {
+            foreach ($this->input as $str2) {
+                if ($str == $str2) {
+                    break;
+                }
+                if ($this->differBy1($str, $str2)) {
+                    for ($i = 0; $i < strlen($str); $i++) {
+                        if ($str[$i] != $str2[$i]) {
+                            $result = str_split($str, 1);
+
+                            //This removes an element at index i
+                            unset($result[$i]);
+
+                            //implode glues together the elements of an array with a given glue
+                            printf("%s\n",implode("", $result));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function differBy1(string $str1, string $str2): bool
+    {
+        $diff = 0;
+        for ($i = 0; $i < strlen($str1); $i++) {
+            if ($str1[$i] != $str2[$i]) {
+                $diff++;
+            }
+            if ($diff >= 2) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     private function solvePart1()
     {
         $twos = $threes = 0;
@@ -29,7 +68,7 @@ class Day02
             $twos += $freq[0];
             $threes += $freq[1];
         }
-        printf("%d * %d = %d", $twos, $threes, $twos * $threes);
+        printf("%d * %d = %d\n", $twos, $threes, $twos * $threes);
     }
 
     private function countLetters(string $s)
@@ -51,6 +90,7 @@ class Day02
     public function main()
     {
         $this->solvePart1();
+        $this->solvePart2();
     }
 
     public function __construct()
