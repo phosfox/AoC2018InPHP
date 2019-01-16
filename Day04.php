8bed2651a195e81169eca32c500e01dd95b2a589
phosfox <mordredur@googlemail.com>
2019-01-16 18:39:08 +0100

    guard is not getting filled with a value?

--- Day04.php
+++ Day04.php
@@ -10,8 +10,8 @@ require_once "IDay.php";
 class Day04 implements IDay
 {
     private $input = [];
-    protected $totals = [];
-    protected $minutes = [[]];
+    private $totals = [];
+    private $minutes = [[]];
 
     private function parseInput(): void
     {
@@ -27,27 +27,43 @@ class Day04 implements IDay
         foreach ($this->input as $line) {
             $m0 = $m1 = 0;
             $guard = 0;
+
             preg_match("/:(\d+)/", $line, $matches);
             $minute = (int)$matches[1];
 
             if (strpos($line, '#') !== false) {
                 preg_match("/#(\d+)/", $line, $matches);
                 $guard = (int)$matches[1];
+                //printf("guard:%d\n", $guard);
 
             } elseif (strpos($line, 'asleep') !== false) {
                 $m0 = $minute;
+                //printf("m0:%d\n", $m0);
+
 
             } elseif (strpos($line, 'wakes') !== false) {
                 $m1 = $minute;
+                //printf("m1:%d\n", $m1);
+
             }
             for ($m = $m0; $m < $m1; $m++) {
-                $this->totals[$guard] += 1;
-                $this->minutes[$guard][$m] += 1;
+                print $guard;
+                if (array_key_exists($guard, $this->totals)) {
+                    $this->totals[$guard]++;
+                } else {
+                    $this->totals[$guard] = 1;
+                }
+                if (array_key_exists($m, $this->minutes[$guard])) {
+                    $this->minutes[$guard][$m] += 1;
+                } else {
+                    $this->minutes[$guard][$m] = 1;
+
+                }
 
             }
         }
-        print_r($this->totals);
-        print_r($this->minutes);
+//        print_r($this->totals);
+//        print_r($this->minutes);
     }
 
 
@@ -63,9 +79,11 @@ class Day04 implements IDay
 
     public function solvePart1(): void
     {
+        print_r($this->totals);
+        print_r($this->minutes);
     }
 
-    public function solvePart2()
+    public function solvePart2(): void
     {
         // TODO: Implement solvePart2() method.
     }
