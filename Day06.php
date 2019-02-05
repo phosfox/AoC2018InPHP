71c19ddfd34787dfba25787a560f23e7d8e74dce
phosfox <mordredur@googlemail.com>
2019-02-05 17:27:53 +0100

    added gitignore and Day06

--- Day06.php
+++ Day06.php
@@ -12,6 +12,11 @@ require_once 'Point.php';
 class Day06 implements IDay
 {
     private $points = [];
+    private $maxX = PHP_INT_MIN;
+    private $maxY = PHP_INT_MIN;
+    private $minX = PHP_INT_MAX;
+    private $minY = PHP_INT_MAX;
+
     public function parseInput(): void
     {
         $filename ='C:\Users\Constantin\PhpstormProjects\AdventOfCode2018\Inputs\Day6.txt';
@@ -28,6 +33,7 @@ class Day06 implements IDay
     public function main()
     {
         $this->parseInput();
+        $this->solvePart1();
     }
 
     public function solvePart2()
@@ -37,7 +43,23 @@ class Day06 implements IDay
 
     public function solvePart1()
     {
-        // TODO: Implement solvePart1() method.
+        foreach ($this->points as $p) {
+            if ($p->x > $this->maxX) {
+                $this->maxX = $p->x;
+            }
+            if ($p->y > $this->maxY) {
+                $this->maxY = $p->y;
+            }
+            if ($p->x < $this->minX) {
+                $this->minX = $p->x;
+            }
+            if ($p->y < $this->minY) {
+                $this->minY = $p->y;
+            }
+        }
+        printf('max: %d,%d min: %d,%d' . PHP_EOL, $this->maxX, $this->maxY, $this->minX, $this->minY);
+
+
     }
 
     public function manhattanDist(int $x1, int $y1, int $x2, int $y2)
